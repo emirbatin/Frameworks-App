c1325dd610c6e5b0fb5dc6ec8f238adc8eb894df
Emir Batın <emirbatinkaraoglan@icloud.com>
2023-11-10 15:12:06 +0300

    Initial Commit

---
+++ Frameworks App/Frameworks_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Frameworks_AppApp.swift
+//  Frameworks App
+//
+//  Created by EBK on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Frameworks_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Frameworks AppUITests/Frameworks_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Frameworks_AppUITestsLaunchTests.swift
+//  Frameworks AppUITests
+//
+//  Created by EBK on 10.11.2023.
+//
+
+import XCTest
+
+final class Frameworks_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
